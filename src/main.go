f5bdc1c41acdbcff3321905e2800412165ba3c11
ArturVargas <arturvargas9226@gmail.com>
2019-12-20 18:24:59 -0600

    [add] controllers

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"./controllers"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,5 +14,12 @@ func main() {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "pong"})
 	})
+	r.GET("/register", controllers.Register)
 	r.Run()
 }
+
+// Explicación
+/*
+	gin.Default() devuelve una instancia de gin engine con los registros de
+	gin.DefaultWriter y el middleware de recuperacion y conectado.
+*/
