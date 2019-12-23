ebd15fbd1ee094f23be4af60ae307846f3f69864
ArturVargas <arturvargas9226@gmail.com>
2019-12-23 15:02:28 -0600

    [update] db config, models and controllers no errors

--- src/main.go
+++ src/main.go
@@ -5,25 +5,13 @@ import (
 
 	config "./Config"
 	controllers "./Controllers"
-	models "./Models"
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-var db = config.DB
-
 func main() {
+
 	fmt.Println("Test go..!")
-
-	db, err := gorm.Open("postgres", config.DbURL(config.BuildDBConfig()))
-	if err != nil {
-		fmt.Println("dbStatus: ", err)
-	}
-
-	defer db.Close()
-
-	db.AutoMigrate(&models.User{})
+	config.Init()
 
 	r := gin.Default()
 
