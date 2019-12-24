4ac7fb25e9c78b026a5339ee655af10252a36851
ArturVargas <arturvargas9226@gmail.com>
2019-12-23 22:06:44 -0600

    [add] crud endpoints

--- src/main.go
+++ src/main.go
@@ -13,28 +13,17 @@ func main() {
 	fmt.Println("Test go..!")
 	config.Init()
 
-	r := gin.Default()
+	router := gin.Default()
 
-	r.POST("/register", controllers.Register)
-	r.GET("/login/:id", controllers.Login)
-	r.Run()
+	router.POST("/register", controllers.Register)
+	router.GET("/login/:id", controllers.Login)
+	router.GET("/benefits", controllers.Benefits)
+	router.GET("/obligations", controllers.ObInfo)
+	router.POST("/benefit", controllers.CreateBenefit)
+	router.POST("/obligation", controllers.CreateOb)
+	router.PUT("/benefit/:id", controllers.UpdateBenefit)
+	router.PUT("/obligation/:id", controllers.UpdateOb)
+	router.DELETE("/benefit/:id", controllers.DeleteBenefit)
+	router.DELETE("/obligation/:id", controllers.DeleteOb)
+	router.Run()
 }
-
-// Explicación
-/*
-	gin.Default() devuelve una instancia de gin engine con los registros de
-	gin.DefaultWriter y el middleware de recuperacion y conectado.
-	donde recibe los endpoints
-*/
-
-// Añadiendo DB PostgreSql.
-/*
-	En Go no existen los objetos pero en su lugar tenemos structs
-	Cree un struct "DBConfig" con las que serán las propiedades de la bd
-	Cree una funcion "buildDBConfig" que sera del tipo struct dentro de esta funcion
-	hago una asignación especial a las propiedades del struct y se guarda en "dbConfig"
-	y retorno su valor para poder usarlo fuera de la funcion.
-	Creo una funcion "dbURL" que recibe un parametro del tipo struct y con "fmt.Sprintf" formateo una
-	cadena de texto.
-	Dentro de la funcion "main" hago la conexion a la bd con gorm.Open.
-*/
